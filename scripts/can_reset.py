c70ca50e80cee3c9d8195f68cc11acb2e2062646
Shayan Moraghebi <99627706+Nominel@users.noreply.github.com>
2025-09-30 16:01:22 -0500

    Handle panda CAN tuples without timestamps

--- scripts/can_reset.py
+++ scripts/can_reset.py
@@ -2,10 +2,11 @@
 import time
 import argparse
 from panda import Panda
+from canfilter import get_all_output_safety_mode
 
 if __name__ == "__main__":
   p = Panda()
-  p.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+  p.set_safety_mode(get_all_output_safety_mode())
 
   while 1:
     if len(p.can_recv()) == 0:
